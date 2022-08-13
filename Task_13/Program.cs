28c65283648b5340559bd8e16f0f22c8a0c942fe
Egor <effrosty@ya.ru>
2022-08-13 23:48:08 +0300

    Task 13 complete

---
+++ Task_13/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задание 13
+
+using static System.Console;
+
+Clear();
+
+Console.Write("Введи число: ");
+
+int ThirdNumber = Convert.ToInt32(Console.ReadLine());
+
+string StringNumber = Convert.ToString(ThirdNumber);
+
+if (StringNumber.Length > 2)
+{
+  Console.WriteLine("третья цифра - " + StringNumber[2]);
+}
+
+else 
+{
+  Console.WriteLine("третьей цифры нет ");
+}
+
+
+
+
+
