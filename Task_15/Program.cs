50f84f3858f095d1aac3ea9d8bf141ffde343e31
Egor <effrosty@ya.ru>
2022-08-14 00:10:15 +0300

    Task 15 complete

---
+++ Task_15/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задание 15
+
+using static System.Console;
+
+Clear();
+
+Write("Введите число дня недели - ");
+
+int day = Convert.ToInt32(ReadLine());
+
+
+void DayNumber (int day)
+{
+    if (day == 6 || day == 7)
+    {
+        WriteLine("Да, этот день выходной!!!");
+    }
+
+    else if (day < 1 || day > 7)
+    {
+        WriteLine("Это число не день недели)))");
+    }
+
+    else 
+    {
+        WriteLine("Нет, данный день недели не является выходным");
+    }
+}
+
+DayNumber(day);
