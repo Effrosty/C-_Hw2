618c5ad5384656ed88e0dcf04cd91b3c64b63823
Egor <effrosty@ya.ru>
2022-08-13 23:18:08 +0300

    task 10 complete

---
+++ Task_10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задание 10
+
+Console.Clear();
+
+Console.Write("Введи трёхзначное число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+string stringNumber = Convert.ToString(a);
+
+Console.WriteLine("вторая цифра этого числа -> " + stringNumber[1]);
+
